cd8f9c9fdb3fbd06ae2a135e97c6d8a6c972094f
zwazoman <gloupik.lemaire@gmail.com>
2024-02-02 11:08:38 +0100

    réparé vraiment

--- Assets/Scripts/TestBoomerang.cs
+++ Assets/Scripts/TestBoomerang.cs
@@ -2,17 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestBoomerang : MonoBehaviour
+public class testBoomerang : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }

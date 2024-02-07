5438603f37fb499b01dbc6d1e9088deb559cd89a
Chorizo <mateoledru05@gmail.com>
2024-02-07 14:09:03 +0100

    Fin de la pause

--- Assets/Scripts/Pause.cs
+++ Assets/Scripts/Pause.cs
@@ -1,3 +1,4 @@
+using System.Data;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -9,21 +10,52 @@ public class Pause : MonoBehaviour
     private PlayerInput _playerInput;
     [SerializeField]
     private GameObject panel_pause;
+    [SerializeField]
+    private GameObject imageUp;
+    [SerializeField]
+    private GameObject imageDown;
 
-    public void OnPause(InputAction.CallbackContext _context)
+    public void OnPause(InputAction.CallbackContext _context) //fct qui lance l'écran de pause
     {
-        Debug.Log("b");
         _playerInput.SwitchCurrentActionMap("UI");
         panel_pause.SetActive(true);
         Time.timeScale = 0;
     }
 
-    public void OnEscape(InputAction.CallbackContext _context)
+    public void ScrollMenuUp(InputAction.CallbackContext _context)//fct qui change l'affichage du joueur pour naviguer dans les menus
     {
-        Debug.Log("b");
+        imageUp.SetActive(true);
+        imageDown.SetActive(false);
+        Accept(_context);
+    }
+
+    public void ScrollMenuDown(InputAction.CallbackContext _context)//fct qui change l'affichage du joueur pour naviguer dans les menus
+    {
+        imageDown.SetActive(true);
+        imageUp.SetActive(false);
+        Quit(_context);
+    }
+
+    public void Accept(InputAction.CallbackContext _context)
+    {
+        if (_context.action.triggered && _context.action.name == "Accept")
+        {
+            OnEscape(_context);
+        }
+    }
+
+    public void Quit(InputAction.CallbackContext _context)
+    {
+        if (_context.action.triggered && _context.action.name == "Quit")
+        {
+            Application.Quit();
+        }
+    }
+
+    public void OnEscape(InputAction.CallbackContext _context)//Fct raccourcie qui permet de revenir au jeu rapidement
+    {
         _playerInput.SwitchCurrentActionMap("Player");
         Time.timeScale = 1;
         panel_pause.SetActive(false);
     }
-
 }

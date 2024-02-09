c33c21241b10dedcfdccff521c0033880c4b7144
zwazoman <gloupik.lemaire@gmail.com>
2024-02-09 10:01:43 +0100

    push tout

---
+++ Assets/Scripts/Boomerang/BoomerangBehaviour.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoomerangBehaviour : MonoBehaviour
+{
+    Rigidbody rb;
+    internal GameObject thrower;
+    [SerializeField] float goSpeed = 10;
+    [SerializeField] float comeBackSpeed;
+    [SerializeField] float boomTime;
+    float fallTime;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        fallTime = boomTime * 1.1f; // fall time plus long pour qu'un joueur ne bougeant pas puisse récupérer automatiquement un boomerang lancé au préalable
+    }
+    
+    private IEnumerator Start()
+    {
+        rb.AddForce(transform.forward * goSpeed); // le boomerang part 
+        yield return new WaitForSeconds(boomTime); // attendre
+        rb.velocity = Vector3.zero; // arrête le boomerang pour ne pas interférer avec la force du retour du boomerang
+        Vector3 towardsPlayer = (thrower.transform.position - transform.position).normalized * comeBackSpeed; // calcul de la force du retour du boomerang vers la position du lanceur
+        rb.AddForce(towardsPlayer); // retour du boomerang
+        yield return new WaitForSeconds(fallTime); // attendre
+        rb.useGravity = true; //a marche pas ? / unfreeze la position en z du boomerang pour qu'il tombe au sol + ralentissement ?
+    }
+    
+    public bool IsGrounded() 
+    {
+        // si le boomerang est en dessous de 1 de hauteur, il est considéré comme au sol
+        if (transform.position.y <= 1) return true;
+        return false;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        // lorsque le boomerang touche une objet
+        if (collision.gameObject.tag == "Player")
+        {
+            // vérifie si l 'objet est un joueur
+            if (collision.gameObject == thrower || IsGrounded())
+            {
+                // vérifie si le joueur est le lanceur OU si le boomerang est au sol et donc inoffenssif
+                collision.gameObject.SendMessage("PickUp");
+                Destroy(gameObject);
+            }
+            else
+            {
+                // si le joueur est un ennemi est que le boomerang est dans les airs, le joueur touché meurt et le lanceur reçoit 1 point
+                collision.gameObject.SendMessage("Kill");
+                thrower.GetComponent<PlayerBoomerang>().ScoreUp();
+            }
+        }
+    }
+}
--- Assets/Scripts/Boomerang/PlayerBoomerang.cs
+++ Assets/Scripts/Boomerang/PlayerBoomerang.cs
@@ -2,27 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerTest : MonoBehaviour
+public class PlayerBoomerang : MonoBehaviour
 {
     public GameObject boomerang;
     public GameObject boomerangTMP;
-    private BoomerangBehaviour throwScript; // a renommer
+    private BoomerangBehaviour boomScript; // a renommer
     public bool hasBoomerang = true; // le joueur a un boomerang ou non
     public float distanceToInstantiate;
-    void Update()
+    
+    internal void ThrowBoomerang()
     {
         if (hasBoomerang)
         {
-            if (Input.GetKeyDown("e"))
-            {
-                print("lance !"); // a retirer
-                boomerangTMP = Instantiate(boomerang,transform.position + transform.forward * distanceToInstantiate, transform.rotation); // instanciation du boomerang
-                throwScript = boomerangTMP.GetComponent<BoomerangBehaviour>(); // a renommer 
-                throwScript.thrower = this.gameObject;
-                hasBoomerang = false;
-            }
-        }  
+            print("lance !"); // a retirer
+            boomerangTMP = Instantiate(boomerang,transform.position + transform.forward * distanceToInstantiate, transform.rotation); // instanciation du boomerang
+            boomScript = boomerangTMP.GetComponent<BoomerangBehaviour>(); // a renommer 
+            boomScript.thrower = this.gameObject;
+            hasBoomerang = false;
+        }
     }
+            
+
     public void ScoreUp()
     {
         // augmente le score quand le message "ScoreUp()" est reçu

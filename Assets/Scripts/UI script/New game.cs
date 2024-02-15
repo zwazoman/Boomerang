c1076b55d8a1bde44d216b8cd8212758e5425c93
Chorizo <mateoledru05@gmail.com>
2024-02-15 15:33:08 +0100

    Fin du responsive

---
+++ Assets/Scripts/UI script/New game.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using Cinemachine;
+
+public class Newgame : MonoBehaviour
+{
+    public GameObject cameraVirtuel;
+    public void New_Game()
+    {
+        cameraVirtuel.SetActive(true);
+    }
+}

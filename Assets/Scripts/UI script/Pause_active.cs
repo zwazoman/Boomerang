89f66a5e0d4ac6357b476113cc1ba915ac96212f
Chorizo <mateoledru05@gmail.com>
2024-02-15 14:43:15 +0100

    Audio

--- Assets/Scripts/UI script/Pause_active.cs
+++ Assets/Scripts/UI script/Pause_active.cs
@@ -1,18 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Pause_active : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void OnPause_active()
     {
-        
+        Time.timeScale = 0;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnPause_desactive()
     {
-        
+        Time.timeScale = 1;
     }
 }

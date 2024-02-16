c6610ce609246fe18e1652543a17186079eaded6
zwazoman <gloupik.lemaire@gmail.com>
2024-02-16 13:41:35 +0100

    noah et fix codes

--- Assets/Scripts/UI script/Start.cs
+++ Assets/Scripts/UI script/Start.cs
@@ -3,13 +3,12 @@ using UnityEngine;
 
 public class _Start : MonoBehaviour
 {
-    public GameObject text1;
-    public GameObject text2;
-    public GameObject text3;
-    public GameObject text4;
-    public GameObject panel;
-    public AudioSource _audio;
-    public AudioSource _audio2;
+    private GameObject text1;
+    private GameObject text2;
+    private GameObject text3;
+    private GameObject text4;
+    private AudioSource source;
+    private AudioSource source2;
 
     public void Awake()
     {
@@ -19,19 +18,19 @@ public class _Start : MonoBehaviour
     IEnumerator _timer()
     {
         yield return new WaitForSeconds(1);
-        _audio.Play();
+        source.Play();
         text1.SetActive(true);
         yield return new WaitForSeconds(1);
         text1.SetActive(false);
-        _audio.Play();
+        source.Play();
         text2.SetActive(true);
         yield return new WaitForSeconds(1);
         text2.SetActive(false);
-        _audio.Play();
+        source.Play();
         text3.SetActive(true);
         yield return new WaitForSeconds(1);
         text3.SetActive(false);
-        _audio2.Play();
+        source2.Play();
         text4.SetActive(true);
         yield return new WaitForSeconds(1);
         text4.SetActive(false);
